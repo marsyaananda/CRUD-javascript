7ad2b512a115070de0a0f82b0903d02ea01ea7ef
marsyaananda <marsyaanandadewi@smkwikrama.sch.id>
2023-11-09 15:44:26 +0700

    crudjs

---
+++ routes/book.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const {
+    getBooks,
+    getBook,
+    addBook,
+    updateBook,
+    deleteBook
+} = require('../controllers/BookController')
+//route untuk menampilkan data
+router.get('/', getBooks)
+
+//route untuk mengirim data
+router.post('/', addBook )
+
+router.get('/:id', getBook)
+
+//route untuk memperbaharui/update data
+router.put('/:id', updateBook)
+
+//route untuk menghapus data
+router.delete('/:id', deleteBook)
+
+module.exports = router
